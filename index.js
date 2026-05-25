838330ecfb384fb2a4ab8b0a5f4015992ff7cf99
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-05-25 11:07:21 +0700

    refactor: move function into function.js

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-hitungAngka();
-const numbers = [];
-function hitungAngka() {
-  if(!Array.isArray(numbers)) {
-    throw new Error("Input harus berupa array");
-  }
-if(numbers.length === 0) {
-  throw new Error("Array kosong");
-}
-if(typeof numbers[0] !== "number") {
-  throw new Error("Input harus berupa angka");
-}
-  let min = numbers[0];
-  let max = numbers[0];
-  let totalNumbers = 0;
-try {
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] < min) {
-    min = numbers[i];
-  }
-  if (numbers[i] > max) {
-    max = numbers[i];
-  }
-  totalNumbers = totalNumbers + numbers[i];
-}
+const { calculateNumbers } = require("./function.js");
 
-average = totalNumbers / numbers.length;
-console.log(min);
-console.log(max)
-console.log(average);
+try {
+  const numbers = [10, 20, 30, 40];
+  const result = calculateNumbers(numbers);
+
+  console.log(result.min);
+  console.log(result.max);
+  console.log(result.average);
 } catch (error) {
-  console.error(`terjadi kesalahan: ${error}`);   
-} 
+  console.error(`terjadi kesalahan: ${error}`);
 }
